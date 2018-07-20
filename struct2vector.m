33ae026c55744ffaa8cbc91ccbbca9ca4bcb57a2
懒麻蛇 <25647407+zh1peng@users.noreply.github.com>
2018-07-20 14:41:51 +0100

    Create struct2vector.m

---
+++ struct2vector.m
@@ -0,0 +1,13 @@
+function varargout=struct2vector(str_in)
+%author: zhipeng caoz@tcd.ie
+%example: [type, lat, urevent]=struct2vector(EEG.event);
+field_names=fieldnames(str_in);
+for i=1:length(field_names)
+for n=1:length(str_in)
+    eval(sprintf('%s(n)=str_in(n).%s;' ,field_names{i},field_names{i}));
+end
+end
+for ii=1:nargout
+varargout{ii}=eval(sprintf('transpose(%s);',field_names{ii}));
+end
+end
